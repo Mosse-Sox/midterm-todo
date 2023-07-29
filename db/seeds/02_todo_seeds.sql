7a8e5c535611a958d682cc9ba7194ae5389e9dd8
Reyna May <roma.reynamayA@gmail.com>
2023-07-29 13:27:21 -0600

    Modify the tables

--- db/seeds/02_todo_seeds.sql
+++ db/seeds/02_todo_seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO todos (name, created_at, completed_at, catagory_id)
-VALUES ('sushi', '1690618520', NULL, 3),
-       ('Harry Potter', '1690618520', NULL, 2),
-       ('The Book of Joy', '1690618520', NULL, 1),
-       ('pencils', '1690618520', NULL, 4);
+INSERT INTO to_dos (name, completed_at, category_id)
+VALUES ('sushi', NULL, 3),
+       ('Harry Potter', NULL, 2),
+       ('The Book of Joy', NULL, 1),
+       ('pencils', NULL, 4);
