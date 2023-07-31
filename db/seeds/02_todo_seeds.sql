cbfbf2cf7ad4e18f99f616da973368fcffbac5fb
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-31 17:36:57 +0000

    changes some seed data to be more bean related

--- db/seeds/02_todo_seeds.sql
+++ db/seeds/02_todo_seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO to_dos (name, completed_at, category_id)
-VALUES ('sushi', NULL, 3),
-       ('Harry Potter', NULL, 2),
-       ('The Book of Joy', NULL, 1),
-       ('pencils', NULL, 4);
+VALUES ('Beans', NULL, 3),
+       ('Mr. Beans Holiday', NULL, 2),
+       ('Jack and the Beanstalk', NULL, 1),
+       ('Bean Shaped Cups', NULL, 4);
